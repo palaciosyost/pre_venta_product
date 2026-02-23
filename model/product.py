716f98d04337627fd605253dc46ff8104221eb23
palaciosyost <palaciosyostin903@gmail.com>
2026-02-23 16:06:11 -0500

    update modulo

--- model/product.py
+++ model/product.py
@@ -8,9 +8,7 @@ class FechaPreventaProduct(models.Model):
     fecha_preventa = fields.Date(string='Fecha de Preventa')
 
 
-    def limpiar_fecha_preventa (self):
-        for product in self:
-            fecha_hoy = fields.Date.today()
-            productos = self.env["product.template"].search([("fecha_preventa", "=", fecha_hoy)])
-            for producto in productos:
-                producto.fecha_preventa = False
+    @api.model
+    def limpiar_fecha_preventa(self):
+        fecha_hoy = fields.Date.today()
+        self.sudo().search([("fecha_preventa", "=", fecha_hoy)]).write({"fecha_preventa": False})
